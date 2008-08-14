4ea3e54b2f209a5f36261dbe8b1bfaa9c9991f7a
netcarver <nc.github@netcarving.com>
2008-08-14 14:08:27 +0800

    Removed plugin template comments

--- ign_password_protect.php
+++ ign_password_protect.php
@@ -1,21 +1,10 @@
 <?php
-// This is a PLUGIN TEMPLATE.
-// Copy this file to a new name like abc_myplugin.php.  Edit the code, then
-// run this file at the command line to produce a plugin for distribution:
-// $ php abc_myplugin.php > abc_myplugin-0.1.txt
-
-// Plugin name is optional.  If unset, it will be extracted from the current
-// file name. Uncomment and edit this line to override:
 $plugin['name'] = 'ign_password_protect';
 $plugin['version'] = '0.5b9';
 $plugin['author'] = 'Jeremy Amos';
 $plugin['author_uri'] = 'http://www.igneramos.com';
 $plugin['description'] = 'Password protect articles or sections; authenticates against txp_users or alternate database (ign_users) ';
 
-// Plugin types:
-// 0 = regular plugin; loaded on the public web side only
-// 1 = admin plugin; loaded on both the public and admin side
-// 2 = library; loaded only when include_plugin() or require_plugin() is called
 $plugin['type'] = '1';
 
 @include_once('zem_tpl.php');
